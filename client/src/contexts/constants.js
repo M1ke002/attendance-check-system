5b218b4a06330c17193c6876982e9c3fd122c8ed
M1ke002 <hoang.n.minh@gmail.com>
2023-08-18 17:17:39 +0700

    small change constants

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -2,10 +2,11 @@ export const apiUrl =
   process.env.NODE_ENV === "production"
     ? "deployed_api"
     : "http://localhost:5000/api";
+const IP_NUMBER = "192.168.1.11"; //change this constant if IP changes
+export const IP = IP_NUMBER + ":3000"; //mobileApiUrl and IP can change after some time?
 export const mobileApiUrl =
   process.env.NODE_ENV === "production"
     ? "deployed_api"
-    : "http://192.168.1.10:5000/api";
-export const IP = "192.168.1.10:3000"; //mobileApiUrl and IP can change after some time?
+    : "http://" + IP_NUMBER + ":5000/api";
 export const ACCESS_TOKEN_NAME = "attendanceSystem";
 export const STUDENT_TOKEN_NAME = "student";
