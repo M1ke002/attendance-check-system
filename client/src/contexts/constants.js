e196394745d4f90a53bc54c0725a56d37c061911
M1ke002 <hoang.n.minh@gmail.com>
2023-12-10 11:18:37 +0700

    Api url bug fix

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,12 +1,12 @@
 export const apiUrl =
   process.env.NODE_ENV === "production"
-    ? "attendance-check-system-server.vercel.app"
+    ? "attendance-check-system-server.vercel.app/api"
     : "http://localhost:5000/api";
 const IP_NUMBER = "192.168.1.11"; //change this constant if IP changes
 export const IP = IP_NUMBER + ":3000"; //mobileApiUrl and IP can change after some time?
 export const mobileApiUrl =
   process.env.NODE_ENV === "production"
-    ? "attendance-check-system-server.vercel.app"
+    ? "attendance-check-system-server.vercel.app/api"
     : "http://" + IP_NUMBER + ":5000/api";
 export const ACCESS_TOKEN_NAME = "attendanceSystem";
 export const STUDENT_TOKEN_NAME = "student";
