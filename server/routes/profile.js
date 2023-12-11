a3ebb5ddaecf82bf40e035868f5c98bff0fdd426
M1ke002 <hoang.n.minh@gmail.com>
2023-12-11 21:14:31 +0700

    Fix bug with folders path when deploy

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -16,7 +16,8 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads/");
+    // cb(null, path.join(process.cwd(), "uploads"));
+    cb(null, `${process.cwd()}/uploads`); //absolute path
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
